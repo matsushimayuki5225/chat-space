f3813d76aff0fbebfde6ac3a55d54d7de71684ad
matsushimayuki5225 <58546697+matsushimayuki5225@users.noreply.github.com>
2019-12-18 15:01:05 +0900

    ユーザー管理

---
+++ db/migrate/20191218031749_create_group_users.rb
@@ -0,0 +1,9 @@
+class CreateGroupUsers < ActiveRecord::Migration[5.0]
+  def change
+    create_table :group_ do |t|
+      t.references :group, foreign_key: true
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
