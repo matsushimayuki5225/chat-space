659e5879770d5607acadd80fbe051893c8109acf
matsushimayuki5225 <58546697+matsushimayuki5225@users.noreply.github.com>
2019-12-19 16:40:24 +0900

    グループ関連

--- db/migrate/20191219015506_create_group_users.rb
+++ db/migrate/20191219015506_create_group_users.rb
@@ -1,6 +1,6 @@
 class CreateGroupUsers < ActiveRecord::Migration[5.0]
   def change
-    create_table :group_ do |t|
+    create_table :group_users do |t|
       t.references :group, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
