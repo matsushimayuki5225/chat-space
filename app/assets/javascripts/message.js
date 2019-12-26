b2151625cea41940894fd62b51bac49234e10d99
matsushimayuki5225 <58546697+matsushimayuki5225@users.noreply.github.com>
2019-12-26 20:16:23 +0900

    自動更新機能編集１

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,49 +1,60 @@
-$(function(){
-  function buildHTML(message){
-    if ( message.image ) {
-      var html =
-       `<div class="chat-home-chat-main__message" data-message-id=${message.id}>  
-          <div class="chat-home-chat-main__message__title">
-            <div class="chat-home-chat-main__message__title__name">
-              ${message.user_name}
-            </div>
-            <div class="chat-home-chat-main__message__title__day">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-          <img src=${message.image} >
-        </div>`
-      return html;
-    } else {
-      var html =
-       `<div class="chat-home-chat-main__message" data-message-id=${message.id}>  
-          <div class="chat-home-chat-main__message__title">
-            <div class="chat-home-chat-main__message__title__name">
-              ${message.user_name}
-            </div>
-            <div class="chat-home-chat-main__message__title__day">
-              ${message.created_at}
-            </div>
-          </div>
-          <div class="lower-message">
-            <p class="lower-message__content">
-              ${message.content}
-            </p>
-          </div>
-        </div>`
-      return html;
+$(function() {
+  var buildHTML = function(message) {
+    if (message.content && message.image) {
+      var html = `<div class="chat-home-chat-main__message" data-message-id=` + message.id + `>` +
+        `<div class="chat-home-chat-main__message__title">` +
+          `<div class="chat-home-chat-main__message__title__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="chat-home-chat-main__message__title__day">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
+    } else if (message.content) {
+      var html = `<div class="chat-home-chat-main__message" data-message-id=` + message.id + `>` +
+        `<div class="chat-home-chat-main__message__title">` +
+          `<div class="chat-homr-chat-main__message__titie__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="chat-home-chat-main__message__title__day">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<p class="lower-message__content">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      var html = `<div class="chat-home-chat-main__message" data-message-id=` + message.id + `>` +
+        `<div class="chat-home-chat-main__message__title">` +
+          `<div class="chat-home-chat-main__message__title__name">` +
+            message.user_name +
+          `</div>` +
+          `<div class="chat-home-chat-main__message__title__day">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<div class="lower-message">` +
+          `<img src="` + message.image + `" class="lower-message__image" >` +
+        `</div>` +
+      `</div>`
     };
-  }
+    return html;
+  };
+
 $('#new_message').on('submit', function(e){
     e.preventDefault()
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    
     $.ajax({
       url: url,
       type: "POST",
@@ -53,6 +64,7 @@ $('#new_message').on('submit', function(e){
       contentType: false
     })
     .done(function(data){
+       console.log(data);
        var html = buildHTML(data);
        $('.chat-home-chat-main').append(html);
        $('form')[0].reset();
@@ -65,5 +77,33 @@ $('#new_message').on('submit', function(e){
     .always(function() {
       $(".chat-home-chat-bottom__box__bottom").prop("disabled", false); 
     });
-  })
+  }); 
+
+    var reloadMessages = function() {
+      last_message_id = $('.message:last').data("message-id");
+      $.ajax({
+        url: "api/messages",
+        type: 'get',
+        dataType: 'json',
+        data: {id: last_message_id}
+      })
+      .done(function(messages) {
+      if (messages.length !== 0) {
+        var insertHTML = '';
+        $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+        });
+        $('.chat-home-chat-main').append(insertHTML);
+        $('.chat-home-chat-main').animate({ scrollTop: $('.chat-home-chat-main')[0].scrollHeight});
+        $("#new_message")[0].reset();
+        $(".form__submit").prop("disabled", false);
+      }
+      })
+      .fail(function() {
+        console.log('error');
+      });
+    };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {  
+  setInterval(reloadMessages, 7000);
+  };
 });
